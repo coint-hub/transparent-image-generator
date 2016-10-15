b9f4e4d12566b8b2494edb8bb5310c15c5d4da56
Sung Min Park <vamf12@gmail.com>
2016-10-15 21:49:29 +0900

    add uwsgi and test.init for test server

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(
     name="tig", version="0.0.1", packages=['tig'],
-    install_requires=['Flask==0.11.1', 'Flask-WTF==0.13.1', 'pypng==0.0.18']
+    install_requires=['Flask==0.11.1', 'Flask-WTF==0.13.1', 'pypng==0.0.18', 'uWSGI==2.0.14']
 )
